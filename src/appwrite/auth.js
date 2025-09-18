9a3ab399c708f241491acb64ca089315f08bc694
SujibShrestha <sujeeb98@gmail.com>
2025-09-18 12:12:42 +0545

    appwrite and redux configured

---
+++ src/appwrite/auth.js
@@ -0,0 +1,71 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+//create
+//login
+//user
+//logout
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create({
+        userId: ID.unique(),
+        email,
+        password,
+        name,
+      });
+      if (userAccount) {
+        return this.login({ email, password }); // account is available login
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession({
+        email,
+        password,
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Error in getCurrentUser", error);
+    }
+
+    return null;
+  }
+
+  async logout(){
+    try {
+      await this.account.deleteSessions()
+    } catch (error) {
+      console.log("Error while logging out",error)
+    }
+  } 
+
+}
+
+const authService = new AuthService(); // creatng a object
+
+export default authService;
---
+++ src/appwrite/config.js
@@ -0,0 +1,135 @@
+import conf from "../conf/conf.js";
+import { Client, TablesDB, ID, Storage, Query } from "appwrite";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket; //Storage
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new TablesDB(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createRow({
+        databaseId: conf.appwriteDatabaseId,
+        collectionId: conf.appwriteTableId,
+        documentId: slug, // use slug as ID or fallback to unique
+        data: {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateRow({
+        databaseId: conf.appwriteDatabaseId,
+        collectionId: conf.appwriteTableId,
+        documentId: slug, // use slug as ID or fallback to unique
+        data: {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        },
+      });
+    } catch (error) {
+      console.log("Error while updating post", error);
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteRow({
+        databaseId: conf.appwriteDatabaseId,
+        collectionId: conf.appwriteTableId,
+        documentId: slug, // use slug as ID or fallback to unique
+      });
+
+      return true;
+    } catch (error) {
+      console.log("error while deleting post", error);
+      return false;
+    }
+  }
+
+  async getPost(slug) {
+    try {
+      return await this.databases.getRow({
+        databaseId: conf.appwriteDatabaseId,
+        collectionId: conf.appwriteTableId,
+        documentId: slug, // use slug as ID or fallback to unique
+      });
+    } catch (error) {
+      console.log("error while getting post", error);
+      return false;
+    }
+  }
+
+  async listPost(queries = [Query.equal("status", "active"), Query.limit(25)]) {
+    try {
+      return await this.databases.listRows({
+        databaseId: conf.appwriteDatabaseId,
+        collectionId: conf.appwriteTableId,
+        queries,
+      });
+    } catch (error) {
+      console.log("error while listing post", error);
+    }
+  }
+
+  //file Services 
+
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile({
+        bucketId: conf.appwriteBucketId,
+        fileId: ID.unique(),
+        file,
+      });
+    } catch (error) {
+      console.log("error while uploading file", error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      return await this.bucket.deleteFile({
+        bucketId: conf.appwriteBucketId,
+        fileId,
+      });
+
+      return true
+    } catch (error) {
+      console.log("error while Deleting file", error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId){
+    return this.bucket.getFilePreview({
+        bucketId: conf.appwriteBucketId,
+        fileId,
+    })
+  }
+
+}
+
+const service = new Service();
+
+export default service;
